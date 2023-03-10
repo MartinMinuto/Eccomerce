ffe5a6689b6ccfa1374bd4874e5615e68847838c
Martin Minuto <martin.minuto1@gmail.com>
2023-03-10 11:57:52 -0300

    AgregarArray

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import NavBar from './components/NavBar/NavBar'
-import ItemlistContainer from './components/Greeting/Greeting'
+
 
 
 
@@ -8,7 +8,6 @@ function App() {
   return (
     <div className="App">
      <NavBar/>
-     <ItemlistContainer Greeting={'Bienvenidos a mi web!!'}/>
     </div>
   );
 }
--- src/components/Greeting/Greeting.js
+++
@@ -1,9 +0,0 @@
-import './Greeting.css'
-
-const ItemlistContainer = ({Greeting}) => {
-  return (
-    <h1 className="greeting">{Greeting}</h1>
-  )
-}
-
-export default ItemlistContainer
