beaf4f50498c0628fe94a0af731926be1d61cfb9
Martin Minuto <martin.minuto1@gmail.com>
2023-03-21 00:15:10 -0300

    Config 00:15

--- src/components/ItemDetails/ItemDetails.js
+++ src/components/ItemDetails/ItemDetails.js
@@ -1,18 +1,28 @@
 import { Link } from 'react-router-dom'
+import './ItemDetails.css'
 
 const ItemDetails = ({id, name, price, img, category}) => {
     return (
         <div>
-           <div>
-                <img src={`${img}`} alt={name}/>
-                <div>
-                    <h1>{name}</h1>
-                    <h2>{category}</h2>
-                    <h3>{price}</h3>
-                    <Link 
-                        to={`./item/${id}`}
-                        >Comprar
-                    </Link>
+           <div className='detailsContainer'>
+                <img className='detailsImg' src={`${img}`} alt={name}/>
+                <div className='detailsSubContainer'>
+                  <div className='detailsPositionContainer'>
+                    <h1 className='detailsName'>{name}</h1>
+                  </div>
+                  <div className='detailsPositionContainer'> 
+                    <h2 className='detailsCategory'>{category}</h2>
+                  </div>  
+                  <div className='detailsPositionContainer'>  
+                    <h3 className='detailsPrice'>$ {price}</h3>
+                  </div>  
+                  <div className='detailsPositionContainer'> 
+                        <Link 
+                            to={`./item/${id}`}
+                            className='detailsBtn'
+                            >Comprar
+                        </Link>
+                   </div> 
                 </div>
             </div>  
         </div>
