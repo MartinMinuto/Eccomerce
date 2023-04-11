05b88ae405a33a33e99903612f86662df0f7cc19
MartinMinuto <martin.minuto1@gmail.com>
2023-04-11 17:46:47 -0300

    config 3.8

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,10 +1,27 @@
 import './Checkout.css'
+import { useContext } from 'react'
+import { CartContext } from '../CartContext/CartContext'
+import { Db } from '../../Services/Firebase/firebaseConfig'
+import { documentId, getDocs, query, collection, where } from 'firebase/firestore'
 
 const Checkout = () => {
-   const handleConfirm = () => { 
-      const objOrder = {
+   const { cart, total } = useContext(CartContext)
 
+   const handleConfirm = async (userData) => { 
+      const objOrder = {
+        buyer: {},
+        items: cart,
+        total: total,
       }
+
+      const ids = cart.map(prod => prod.id)
+
+      const productsRef = query(collection(Db, 'products'), where(documentId(), 'in', ids))
+      
+      const productsAdd = await getDocs(productsRef)
+
+      const { docs } = productsAdd
+
     }  
 
 
