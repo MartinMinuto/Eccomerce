6721baa87af8ae5c7d3322c193cd3eb370880e2a
MartinMinuto <martin.minuto1@gmail.com>
2023-04-12 19:07:31 -0300

    Config 3.11

--- src/Services/Firebase/firebaseConfig.js
+++ src/Services/Firebase/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBErkMJt4jT6g7vRYq0f4R9QD0vjOBGXyQ",
-  authDomain: "backendecommerce01.firebaseapp.com",
-  projectId: "backendecommerce01",
-  storageBucket: "backendecommerce01.appspot.com",
-  messagingSenderId: "436932899267",
-  appId: "1:436932899267:web:067b38289457decaa50d76"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
